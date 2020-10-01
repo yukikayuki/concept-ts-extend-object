a2184ef03502349315d05a05397cab3b2156def2
yukikayuki <yukikayuki@gmail.com>
2020-10-01 23:36:16 +0900

    objectと振る舞いを別々で定義し、合成する手法のサンプルを作成

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,19 @@
-import {add} from "./index";
+import {extendUserWithBehavior, User} from './index'
 
-it('add', () => {
-  expect(add(1, 2)).toBe(3)
+describe('extendUserWithBehavior', () => {
+  it('userに振る舞いを追加できること', () => {
+    const userObject: User = {
+      id: 1,
+      firstName: 'Taro',
+      lastName: 'Tanaka',
+      age: 15,
+    }
+    const user = extendUserWithBehavior(userObject)
+
+    expect(user.id).toBe(1)
+    expect(user.firstName).toBe('Taro')
+    expect(user.lastName).toBe('Tanaka')
+    expect(user.age).toBe(15)
+    expect(user.getFullName()).toBe('Taro Tanaka')
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-export function add(a: number, b: number) {
-  return a + b
+export type User = {
+  id: number
+  firstName: string
+  lastName: string
+  age: number
+}
+
+class UserBehavior {
+  get user() {
+    return (this as any) as User
+  }
+
+  getFullName() {
+    const {user} = this
+    return `${user.firstName} ${user.lastName}`
+  }
+}
+
+export type UserWithBehavior = User & UserBehavior
+
+export function extendUserWithBehavior(user: User): UserWithBehavior {
+  return Object.assign<UserBehavior, User>(new UserBehavior(), user)
 }
